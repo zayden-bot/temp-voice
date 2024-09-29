31205cc4d109ab3eb877046dcf64155289edc4e1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-09-30 00:31:49 +0100

    Refactor VoiceStateCache to update voice state and handle user removal

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,45 @@ use error::Result;
 
 pub struct State {
     pub channel_id: Option<ChannelId>,
-    pub guild_id: GuildId,
+    pub guild_id: Option<GuildId>,
     pub user: User,
 }
 
+impl State {
+    async fn from_voice_state(ctx: &Context, state: VoiceState) -> Result<Self> {
+        let user = if let Some(member) = state.member {
+            member.user
+        } else {
+            ctx.http.get_user(state.user_id).await?
+        };
+
+        Ok(Self {
+            channel_id: state.channel_id,
+            guild_id: state.guild_id,
+            user,
+        })
+    }
+}
+
 pub struct VoiceStateCache;
+
+impl VoiceStateCache {
+    pub async fn update(ctx: &Context, new: VoiceState) -> Result<Option<State>> {
+        let new = new.clone();
+        let mut data = ctx.data.write().await;
+        let cache = data
+            .get_mut::<Self>()
+            .expect("Expected VoiceStateCache in TypeMap");
+
+        let old = if new.channel_id.is_none() {
+            cache.remove(&new.user_id)
+        } else {
+            cache.insert(new.user_id, State::from_voice_state(ctx, new).await?)
+        };
+
+        Ok(old)
+    }
+}
 
 impl TypeMapKey for VoiceStateCache {
     type Value = HashMap<UserId, State>;
