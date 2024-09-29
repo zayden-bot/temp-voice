d525ad2988ee3fb7a7bcc7bb9b3e8ed04edb024c
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-09-29 22:57:34 +0100

    Refactor error handling and implement ErrorResponse trait
    - Made `ErrorResponse` trait public in `zayden_core` module
    - Added implementation of `ErrorResponse` trait for `Error` enum
    - Updated `Error` enum to include specific error messages
    - Implemented `Display` and `Error` traits for `Error` enum
    - Converted `serenity::Error` into `Error` using `From` trait

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use zayden_core::ErrorResponse;
+
 pub(crate) type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
@@ -8,6 +10,29 @@ pub enum Error {
 
     Serenity(serenity::Error),
 }
+
+impl ErrorResponse for Error {
+    fn to_response(&self) -> String {
+        match self {
+            Error::CommandNotInGuild => String::from("This command can only be used in a guild."),
+            Error::MemberNotInVoiceChannel => {
+                String::from("You must be in a voice channel to use this command.")
+            }
+            Error::MissingPermissions => {
+                String::from("Only the channel owner can use this command.")
+            }
+            _ => String::new(),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for Error {}
 
 impl From<serenity::Error> for Error {
     fn from(error: serenity::Error) -> Self {
