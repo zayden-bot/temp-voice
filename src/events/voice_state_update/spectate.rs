7c45fdb461cc05bd33c21c8b48dfbe3edcdfd9f4
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2025-02-15 03:18:33 +0000

    Refactor spectate function to remove unused parameters and streamline member editing logic

--- src/events/voice_state_update/spectate.rs
+++ src/events/voice_state_update/spectate.rs
@@ -1,24 +1,16 @@
 use serenity::all::{ChannelId, Context, EditMember, VoiceState};
 use sqlx::{Database, Pool};
 
-use crate::{voice_channel_manager::VoiceChannelMode, CachedState, Result, VoiceChannelManager};
+use crate::{voice_channel_manager::VoiceChannelMode, Result, VoiceChannelManager};
 
 pub async fn spectate<Db: Database, Manager: VoiceChannelManager<Db>>(
     ctx: &Context,
     pool: &Pool<Db>,
     new: &VoiceState,
-    old: Option<&CachedState>,
 ) -> Result<()> {
     if let Some(channel_id) = new.channel_id {
         on_join::<Db, Manager>(ctx, pool, new, channel_id).await;
         return Ok(());
-    }
-
-    if let Some(old) = old {
-        if let Some(channel_id) = old.channel_id {
-            on_leave::<Db, Manager>(ctx, pool, old, channel_id).await;
-            return Ok(());
-        }
     }
 
     Ok(())
@@ -34,33 +26,15 @@ async fn on_join<Db: Database, ChannelManager: VoiceChannelManager<Db>>(
         return;
     };
 
-    if !matches!(data.mode, VoiceChannelMode::Spectator) {
-        return;
-    }
-
     let guild_id = new.guild_id.unwrap();
-    guild_id
-        .edit_member(ctx, new.user_id, EditMember::new().mute(true))
-        .await
-        .unwrap();
-}
 
-async fn on_leave<Db: Database, ChannelManager: VoiceChannelManager<Db>>(
-    ctx: &Context,
-    pool: &Pool<Db>,
-    old: &CachedState,
-    channel_id: ChannelId,
-) {
-    let Some(data) = ChannelManager::get(pool, channel_id).await.unwrap() else {
-        return;
+    let builder = match data.mode {
+        VoiceChannelMode::Spectator => EditMember::new().mute(false),
+        _ => EditMember::new().mute(true),
     };
 
-    if !matches!(data.mode, VoiceChannelMode::Spectator) {
-        return;
-    }
-
-    old.guild_id
-        .edit_member(ctx, old.user_id, EditMember::new().mute(false))
+    guild_id
+        .edit_member(ctx, new.user_id, builder)
         .await
         .unwrap();
 }
